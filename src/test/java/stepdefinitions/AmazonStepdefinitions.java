b9bb3b44e4c99ea84892087e8b0e0bf4bdab255a
abdullahyildiz52 <ce_abdullahyildiz@hotmail.com>
2023-07-16 17:27:45 +0300

    Url de  parametre olarak urun arama

--- src/test/java/stepdefinitions/AmazonStepdefinitions.java
+++ src/test/java/stepdefinitions/AmazonStepdefinitions.java
@@ -11,7 +11,7 @@ import utilities.Driver;
 
 public class AmazonStepdefinitions {
 
-    AmazonPage amazonPage=new AmazonPage();
+    AmazonPage amazonPage = new AmazonPage();
 
     @Given("kullanici amazon anasayfaya gider")
     public void kullanici_amazon_anasayfaya_gider() {
@@ -19,20 +19,23 @@ public class AmazonStepdefinitions {
         Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
 
     }
+
     @Then("arama kutusuna Nutella Yazip ENTER tusuna basar")
     public void arama_kutusuna_nutella_yazip_enter_tusuna_basar() {
         // Write code here that turns the phrase above into concrete actions
-        amazonPage.aramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
+        amazonPage.aramaKutusu.sendKeys("Nutella" + Keys.ENTER);
 
     }
+
     @Then("arama sonuclarinin Nutella icerdigini test eder")
     public void arama_sonuclarinin_nutella_icerdigini_test_eder() {
         // Write code here that turns the phrase above into concrete actions
-        String expectedIcerik="Nutella";
-        String actualSonucYazisi=amazonPage.sonucYaziElementi.getText();
+        String expectedIcerik = "Nutella";
+        String actualSonucYazisi = amazonPage.sonucYaziElementi.getText();
         Assert.assertTrue(actualSonucYazisi.contains(expectedIcerik));
 
     }
+
     @Then("sayfayi kapatir")
     public void sayfayi_kapatir() {
         // Write code here that turns the phrase above into concrete actions
@@ -43,27 +46,58 @@ public class AmazonStepdefinitions {
 
     @Then("arama kutusuna Samsung Yazip ENTER tusuna basar")
     public void aramaKutusunaSamsungYazipENTERTusunaBasar() {
-        amazonPage.aramaKutusu.sendKeys("Samsung"+ Keys.ENTER);
+        amazonPage.aramaKutusu.sendKeys("Samsung" + Keys.ENTER);
 
     }
 
-    @And("arama sonuclarini Samsung icerdigini test eder")
+    @And("arama sonuclarinin Samsung icerdigini test eder")
     public void aramaSonuclarininSamsungIcerdiginiTestEder() {
-        String expectedIcerik="Samsung";
-        String actualSonucYazisi=amazonPage.sonucYaziElementi.getText();
+        String expectedIcerik = "Samsung";
+        String actualSonucYazisi = amazonPage.sonucYaziElementi.getText();
         Assert.assertTrue(actualSonucYazisi.contains(expectedIcerik));
     }
 
 
     @Then("arama kutusuna Java Yazip ENTER tusuna basar")
     public void aramaKutusunaJavaYazipENTERTusunaBasar() {
-        amazonPage.aramaKutusu.sendKeys("Java"+ Keys.ENTER);
+        amazonPage.aramaKutusu.sendKeys("Java" + Keys.ENTER);
     }
 
     @And("arama sonuclarinin Java icerdigini test eder")
     public void aramaSonuclariniJavaIcerdiginiTestEder() {
-        String expectedIcerik="Java";
-        String actualSonucYazisi=amazonPage.sonucYaziElementi.getText();
+        String expectedIcerik = "Java";
+        String actualSonucYazisi = amazonPage.sonucYaziElementi.getText();
         Assert.assertTrue(actualSonucYazisi.contains(expectedIcerik));
+    }
+
+    @Then("arama kutusuna Nutella yazip, ENTER tusuna basar")
+    public void aramaKutusunaNutellaYazipENTERTusunaBasar() {
+        amazonPage.aramaKutusu.sendKeys("Nutella" + Keys.ENTER);
+    }
+
+    @Then("arama kutusuna {string} yazip, ENTER tusuna basar")
+    public void aramaKutusunaYazipENTERTusunaBasar(String arananIcerik) {
+        amazonPage.aramaKutusu.sendKeys(arananIcerik+Keys.ENTER);
+    }
+
+    @And("arama sonuclarinin {string} icerdigini test eder")
+    public void aramaSonuclarininIcerdiginiTestEder(String aranacakIcerik) {
+        String actualSonucYazisi = amazonPage.sonucYaziElementi.getText();
+        Assert.assertTrue(actualSonucYazisi.contains(aranacakIcerik));
+    }
+
+    @And("{int} saniye bekler")
+    public void saniyeBekler(int beklemeSuresi) throws InterruptedException {
+        try {
+            Thread.sleep(beklemeSuresi*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Given("kullanici {string} anasayfaya gider")
+    public void kullaniciAnasayfayaGider(String istenenUrl) {
+        Driver.getDriver().get(ConfigReader.getProperty(istenenUrl));
     }
 }
